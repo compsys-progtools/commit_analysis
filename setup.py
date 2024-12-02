c378362c86425a92212cffe63c8d82e92d8525c4
AidanL19 <144722248+AidanL19@users.noreply.github.com>
2024-12-02 13:24:49 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup (
+    name = 'commit_analysis',
+    version = '0.1.0',
+    py_modules = ['commit_analysis'],
+    install_requires = [
+        'Click', 
+        'pandas',
+        'matplotlib',
+        'myst-parser', 
+    ],
+    entry_points = {
+        'console_scripts': [
+            'commit_analysis = commit_analysis:main',
+        ],
+    ),
+    include_package_data = True,
+)
