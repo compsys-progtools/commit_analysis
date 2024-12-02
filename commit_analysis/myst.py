f09db0248b2bf8c17eba3a7914c68a19ecd87d6b
AidanL19 <144722248+AidanL19@users.noreply.github.com>
2024-12-02 17:53:03 -0500

    Update myst.py

--- commit_analysis/myst.py
+++ commit_analysis/myst.py
@@ -1,1 +1,15 @@
+import subprocess
+import os
 
+def generate_myst_page(notebook_ipynb, report_md, output_dir):
+    convert_notebook_to_markdown(notebook_ipynb, report_md)
+    start_myst_server(output_dir)
+
+def convert_notebook_to_markdown(notebook_path, markdown_path):
+    command = ['jupytext', '--to', 'markdown', notebook_path, '-o', markdown_path]
+    subprocess.run(command, check=True)
+
+def start_myst_server(directory):
+    os.chdir(directory)
+    command = ['myst', 'start']
+    subprocess.Popen(command)
