ba03675b01ce34c518c169aaa87564bea242da81
AidanL19 <144722248+AidanL19@users.noreply.github.com>
2024-12-03 16:08:34 -0500

    Update myst_generator.py

--- commit_analysis/myst_generator.py
+++ commit_analysis/myst_generator.py
@@ -7,8 +7,8 @@ def generate_myst_page(notebook_ipynb, report_md, output_dir):
 
 def convert_notebook_to_markdown(notebook_path, markdown_path):
     command = ['jupytext', '--to', 'markdown', notebook_path, '-o', markdown_path]
-    subprocess.run(command, check=True)
+    subprocess.run(command, check = True)
 
 def start_myst_server(directory):
-    command = ['myst', 'start']
-    subprocess.run(command, cwd=directory)
+    command = ['python', '-m', 'myst.cli', 'start']
+    subprocess.run(command, cwd = directory)
