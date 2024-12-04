feec765956fcf61ed57957ab3a9351aecbb9e85a
AidanL19 <144722248+AidanL19@users.noreply.github.com>
2024-12-03 20:47:25 -0500

    Update generator.py

--- commit_analysis/generator.py
+++ commit_analysis/generator.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import textwrap
 
 def generate_page(notebook_ipynb, report_md, output_dir):
     convert_notebook_to_markdown(notebook_ipynb, report_md)
@@ -18,18 +19,29 @@ def convert_notebook_to_markdown(notebook_path, markdown_path):
 
 def build_jupyter_book(directory):
     config_path = os.path.join(directory, '_config.yml')
-    config_content = """
-        title: Commit Analysis Report
-        execute:
-        execute_notebooks: force
-    """
+    config_content = textwrap.dedent("""
+    title: Commit Analysis Report
+    execute:
+      execute_notebooks: force
+    sphinx:
+      config:
+        suppress_warnings:
+          - myst.header
+          - myst.xref_missing
+          - etoc.tableofcontents
+    """)
     
     with open(config_path, 'w') as f:
         f.write(config_content)
 
     toc_path = os.path.join(directory, '_toc.yml')
+    toc_content = textwrap.dedent("""
+    format: jb-book
+    root: report
+    """)
+    
     with open(toc_path, 'w') as f:
-        f.write("format: jb-book\nroot: report\n")
+        f.write(toc_content)
 
-    command = ['jupyter-book', 'build', directory]
+    command = ['jupyter-book', 'build', directory, '--quiet']
     subprocess.run(command, check = True)
